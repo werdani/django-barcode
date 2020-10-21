dd74b30273bbb0184deba73f21e7ddafd20795ef
werdani <https://github.com/werdani/django-job-board.git>
2020-10-21 10:24:09 +0200

    the finaly

--- barcode_proj/urls.py
+++ barcode_proj/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from barcode_proj import settings
+from django.conf import settings
 from django.conf.urls.static import static
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]+ static(settings.MEDIA_URL, document_rooot=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
